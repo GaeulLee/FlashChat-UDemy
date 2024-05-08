5e19f5b7c90bf73f8b18971ad82f100ee18b92c7
gaeulLee <rhakq23@gmail.com>
2024-05-08 20:48:10 +0900

    use cocoapod for practice

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,15 +7,30 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-       
+        
+        // titleAnimation()
+        // 위의 함수와 동일한 효과를 주는 코코아팟 사용
+        titleLabel.text = "⚡️FlashChat"
+    }
+    
+    func titleAnimation(){
+        titleLabel.text = ""
+        var strIndex = 0.0
+        let titleText = "⚡️FlashChat"
+        for str in titleText {
+            Timer.scheduledTimer(withTimeInterval: 0.15 * strIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(str)
+            }
+            strIndex += 1.0
+        }
     }
     
 
